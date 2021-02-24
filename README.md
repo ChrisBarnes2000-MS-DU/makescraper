903c3d4a78b2b9e359a028aa684abc65b92046ac
Christopher Barnes <chris.barnes.2000@me.com>
2021-02-23 21:25:29 -0800

    [Refactor] c.onHTML, data struct, and website being scrapped

--- README.md
+++ README.md
@@ -44,18 +44,18 @@ _Complete each task in the order they appear. Use [GitHub Task List](https://hel
 
 - [X] **IMPORTANT**: Complete the Web Scraper Workflow worksheet distributed in class.
 - [X] Create a `struct` to store your data.
-- [ ] Refactor the `c.OnHTML` callback on line `16` to use the selector(s) you tested while completing the worksheet.
-- [ ] Print the data you scraped to `stdout`.
+- [X] Refactor the `c.OnHTML` callback on line `16` to use the selector(s) you tested while completing the worksheet.
+- [X] Print the data you scraped to `stdout`.
 
 ##### Stretch Challenges
 
-- [ ] Add more fields to your `struct`. Extract multiple data points from the website. Print them to `stdout` in a readable format.
+- [X] Add more fields to your `struct`. Extract multiple data points from the website. Print them to `stdout` in a readable format.
 
 #### Serializing & Saving
 
-- [ ] Serialize the `struct` you created to JSON. Print the JSON to `stdout` to validate it.
-- [ ] Write scraped data to a file named `output.json`.
-- [ ] **Add, commit, and push to GitHub**.
+- [X] Serialize the `struct` you created to CSV. Print to `stdout` or Open the CSV to validate it.
+- [X] Write scraped data to a file named `Chess_Tournaments.csv`.
+- [X] **Add, commit, and push to GitHub**.
 
 ## Resources
 
--- scrape.go
+++ scrape.go
@@ -1,32 +1,74 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
-// DataProcessing ... Stores the contents of file as a string
-type DataProcessing struct {
-	Title            string
-	Image            string
-	SourceCodelink   string
-	PrimarySection   string
-	SecondarySection string
+type data = struct {
+	Name         string
+	ClockTimes   string
+	Type         string
+	Rated        string
+	TotalTime    string
+	Winner       string
+	TotalPlayers string
+	Link         string
+	Date         string
+}
+
+func createCSV(fileName string, header []string) csv.Writer {
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", fileName, err)
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write CSV header
+	writer.Write(header)
+
+	return *writer
 }
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
+	fileName := "Chess_Tournaments.csv"
+	ChessTournaments := createCSV(fileName, []string{"Name", "Clock Times", "Type", "Rated", "Total Time", "Winner", "Total Players", "Link", "Date"})
+
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
+	c.OnHTML("#main-wrap > main > div > div > table > tbody > tr", func(e *colly.HTMLElement) {
+		Name := e.ChildText("td.header > a > span.name")
+		items := strings.Split(e.ChildText("td.header > a > span:nth-child(2)"), " • ")
+		ClockTimes := items[0]
+		Type := items[1]
+		Rated := items[2]
+		TotalTime := items[3]
+		Winner := e.ChildText("td.winner")
+		TotalPlayers := e.ChildText("td.text")
+		id := e.ChildAttr("td.header > a", "href")
+		Link := "https://lichess.org" + id
+		Date := e.ChildAttr("td:nth-child(3) > time", "datetime")
 
-		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		scrappedData := data{Name, ClockTimes, Type, Rated, TotalTime, Winner, TotalPlayers, Link, Date}
+
+		ChessTournaments.Write([]string{Name, ClockTimes, Type, Rated, TotalTime, Winner, TotalPlayers, Link, Date})
+
+		// fmt.Printf("Found: %q\n", e.Text)
+		fmt.Printf("Found: %q\n", scrappedData)
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong:", err)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -34,6 +76,8 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	// Start scraping on
+	c.Visit("https://lichess.org/@/cbarnes2000/tournaments/created")
+
+	log.Printf("Scraping finished, check file %q for results\n", fileName)
 }
